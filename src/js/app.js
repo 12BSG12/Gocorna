8f148336554fa0f39b0ee32f168025ae20ec0b8b
12BSG12 <carlj8335@gmail.com>
2022-07-16 21:17:32 +0500

    full header + adaptive

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,14 @@
 import * as flsFunctions from "./modules/functions.js";
 import "./modules/slick.js";
-import "../../node_modules/jquery/dist/jquery.js";
 
 flsFunctions.isWebp();
+
+flsFunctions.ibg();
+
+let header__burger = document.querySelector('.header__burger');
+let menu = document.querySelector('.menu');
+
+header__burger.onclick = function(){
+    header__burger.classList.toggle('active');
+    menu.classList.toggle('active');
+}
